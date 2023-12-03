9f252988180f3c0b82b3b3f89aec25973a329b91
typesafety <ailoodee@gmail.com>
2023-12-03 03:01:38 +0100

    Add day 2 part 2

--- src/Solutions/Day02.hs
+++ src/Solutions/Day02.hs
@@ -9,7 +9,11 @@ import MyPrelude
 
 import Data.List qualified as List
 
-import Optics
+import Optics (
+    view,
+    (^.),
+    over,
+    )
 import Text.Megaparsec qualified as P
 import Text.Megaparsec.Char qualified as P
 import Text.Megaparsec.Char.Lexer qualified as Lex
@@ -24,29 +28,32 @@ solve1 = showt
     . partialParse inputP
   where
     gameValid :: Game -> Bool
-    gameValid = all cubesValid . view #cubeReveals
+    gameValid = all cubeSetValid . view #cubeSets
       where
-        cubesValid :: [(Int, Color)] -> Bool
-        cubesValid = all cubeRevealValid
-          where
-            cubeRevealValid :: (Int, Color) -> Bool
-            cubeRevealValid (n, color) =
-                let condition = case color of
-                        Red -> (<= 12)
-                        Green -> (<= 13)
-                        Blue -> (<= 14)
-                in condition n
+        cubeSetValid :: CubeSet -> Bool
+        cubeSetValid cs = and @[] [
+            view #red cs <= 12,
+            view #green cs <= 13,
+            view #blue cs <= 14
+            ]
 
 data Game = Game {
     id :: Int,
-    cubeReveals :: [[(Int, Color)]]
+    cubeSets :: [CubeSet]
+    }
+    deriving (Eq, Show, Generic)
+
+data CubeSet = CubeSet {
+    red :: Int,
+    green :: Int,
+    blue :: Int
     }
     deriving (Eq, Show, Generic)
 
 data Color = Red | Green | Blue
     deriving (Eq, Show)
 
--- * Parsing
+-- ** Parsing
 
 type Parser = P.Parsec Void Text
 
@@ -56,11 +63,22 @@ inputP = gameP `P.sepEndBy` P.eol
     gameP :: Parser Game
     gameP = do
         id <- P.string "Game" *> P.hspace1 *> Lex.decimal <* P.char ':' <* P.hspace
-        cubes <- cubeSetP `P.sepBy` (P.char ';' *> P.hspace)
-        pure (Game id cubes)
+        cubeSets <- cubeSetP `P.sepBy` (P.char ';' *> P.hspace)
+        pure (Game id cubeSets)
 
-    cubeSetP :: Parser [(Int, Color)]
-    cubeSetP = cubeP `P.sepBy` (P.char ',' *> P.hspace)
+    cubeSetP :: Parser CubeSet
+    cubeSetP = toCubeSet <$> cubeP `P.sepBy` (P.char ',' *> P.hspace)
+      where
+        toCubeSet :: [(Int, Color)] -> CubeSet
+        toCubeSet = foldl' (flip addToCubeSet) (CubeSet 0 0 0)
+          where
+            addToCubeSet :: (Int, Color) -> CubeSet -> CubeSet
+            addToCubeSet (n, color) cs =
+                let modify = case color of
+                        Red -> over #red
+                        Green -> over #green
+                        Blue -> over #blue
+                in modify (+ n) cs
 
     cubeP :: Parser (Int, Color)
     cubeP = do
@@ -78,4 +96,30 @@ inputP = gameP `P.sepEndBy` P.eol
 -- * Part 2
 
 solve2 :: Text -> Text
-solve2 = todo
+solve2 =
+    showt
+    . sum
+    . fmap (cubeSetPower . getCubeSet . mconcat . fmap CubeSetMax . view #cubeSets)
+    . partialParse inputP
+  where
+    cubeSetPower :: CubeSet -> Int
+    cubeSetPower cs = product @[] [cs ^. #red, cs ^. #green, cs ^. #blue]
+
+-- | Newtype for defining specific semigroup instance.
+newtype CubeSetMax = CubeSetMax CubeSet
+    deriving (Eq, Show)
+
+getCubeSet :: CubeSetMax -> CubeSet
+getCubeSet (CubeSetMax cs) = cs
+
+instance Semigroup CubeSetMax where
+    (<>) :: CubeSetMax -> CubeSetMax -> CubeSetMax
+    CubeSetMax cs1 <> CubeSetMax cs2 = CubeSetMax $ CubeSet {
+        red = max (cs1 ^. #red) (cs2 ^. #red),
+        green = max (cs1 ^. #green) (cs2 ^. #green),
+        blue = max (cs1 ^. #blue) (cs2 ^. #blue)
+        }
+
+instance Monoid CubeSetMax  where
+    mempty :: CubeSetMax
+    mempty = CubeSetMax (CubeSet 0 0 0)
